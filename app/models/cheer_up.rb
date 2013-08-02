99b71ae9cbb93ca48bb7b398db6637a934486654
James Elliott <elliottjam@gmail.com>
2013-08-02 16:31:43 +0100

    second carrierwave, images upload on cheerups working

--- app/models/cheer_up.rb
+++ app/models/cheer_up.rb
@@ -1,4 +1,8 @@
 class CheerUp < ActiveRecord::Base
+  require 'carrierwave/processing/rmagick'
+  mount_uploader :image_upload, AvatarUploader
+
+
   attr_accessible :content, :image_upload, :latitude, :longitude, :rating, :sound_upload, :user_id, :votes
 
   belongs_to :user
