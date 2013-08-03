43d5ae267b112474b159b1a5abdbcd035c59064b
Ankoor Patel <ankoor.p@gmail.com>
2013-08-03 15:58:40 +0100

    add function to pull CheerUps and User details along with either model. Add function to vote up and vote down a cheerup

--- app/models/cheer_up.rb
+++ app/models/cheer_up.rb
@@ -5,4 +5,21 @@ class CheerUp < ActiveRecord::Base
 
   validates :content, presence: true
 
+  scope :blurb, -> { select("content, image_upload, rating, sound_upload, user_id, votes") }
+
+  scope :with_user, -> { includes(:user)}
+
+  def vote_up
+    @cheer_up.rating += 1
+    @cheer_up.votes += 1
+    @cheer_up.save
+  end
+
+  def vote_down
+    @cheer_up.rating -= 1
+    @cheer_up.votes += 1
+    @cheer_up.save
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :cheer_ups
 
+  scope :blurb, -> { select("users.id, users.firstname, users.image_upload") }
+
+  scope :with_cu, -> { includes(:cheer_ups)}
+
   mount_uploader :image_upload, AvatarUploader
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -17,5 +21,16 @@ class User < ActiveRecord::Base
   validates :lastname, presence: true
   validates :bio, presence: true, :on => :update
   validates :image_upload, presence: true, :on => :update
+
+  def stats
+    number_of_CU = cheer_ups.count
+    sum_CU_ratings = cheer_ups.map { |cu|
+      cu['rating'] }.reduce(0, :+)
+    number_of_CU_votes = cheer_ups.map { |cu|
+      cu['votes'] }.reduce(0, :+)
+    average_CU_rating = sum_CU_ratings / number_of_CU
+
+    stats = {number_of_CU: number_of_CU, average_CU_rating: average_CU_rating, number_of_CU_votes: number_of_CU_votes }
+  end
 
 end
