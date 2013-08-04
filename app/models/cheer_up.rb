d0c2c1c6a1fa8819bf773229e01ffa93ea3d558c
James Elliott <elliottjam@gmail.com>
2013-08-04 17:31:08 +0100

    sounduploader added

--- app/models/cheer_up.rb
+++ app/models/cheer_up.rb
@@ -1,6 +1,7 @@
 class CheerUp < ActiveRecord::Base
   require 'carrierwave/processing/rmagick'
   mount_uploader :image_upload, AvatarUploader
+  mount_uploader :sound_upload, CheersoundUploader
 
 
   attr_accessible :content, :image_upload, :latitude, :longitude, :rating, :sound_upload, :user_id, :votes
