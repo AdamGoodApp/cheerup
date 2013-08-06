e9d767cb901577cfd4a7fc7262a020bb839928a3
Adam <Adam@Natashas-MacBook.local>
2013-08-06 15:41:56 +0100

    Add random background color and only show cu image if there is one

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def randomized_background_image
+  images = ["cubg1.png", "cubg2.png", "cubg3.png", "cubg5.png", "cubg6.png", "cubg7.png", "cubg8.png", "cubg9.png","cubg10.png", "cubg11.png", "cubg12.png", "cubg13.png", "cubg14.png", "cubg15.png","cubg16.png", "cubg17.png", "cubg18.png", "cubg19.png", "cubg20.png", "cubg21.png"]
+  images.sample
+end
 end
