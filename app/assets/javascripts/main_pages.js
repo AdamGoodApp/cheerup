a96deef49e22213df0a9a893fa3534f3b8bfffed
Ankoor Patel <ankoor@Ankoors-MacBook-Pro.local>
2013-08-08 00:37:14 +0100

    cu feed styled with background colors and adjusted layout

--- app/assets/javascripts/main_pages.js
+++ app/assets/javascripts/main_pages.js
@@ -6,7 +6,7 @@ $(document).ready(init);
 function init() {
     $('#cu_feed').isotope({
   masonry: {
-    columnWidth: 240
+    // columnWidth: 240
   }
 });
 
@@ -31,14 +31,12 @@ function init() {
 
     // });
 
-
-
-
-
-
     // when everything loads, make the "all" options selected
     $('.filter a[data-filter-value=""]').addClass('selected');
 
+    $(".cheer_up").each(function() {
+        $(this).css("background-color", get_random_color());
+    });
     // filter buttons
     $('.filter a').click(
     function(e){
@@ -72,9 +70,40 @@ function init() {
 
         // run the filter on the isotope element
         things.isotope({ filter: selector });
-    }
-  );
-
-
+    });
 
 }
+
+    var color_set = new Array();
+        color_set[0] = '17FF83';
+        color_set[1] = '2AD3E8';
+        color_set[2] = '6193FF';
+        color_set[3] = 'AC63E8';
+        color_set[4] = 'FF3792';
+        color_set[5] = '2EC540';
+        color_set[6] = 'FFDB3A';
+        color_set[7] = 'FF6430';
+        color_set[8] = 'F03323';
+        color_set[9] = '023E73';
+        color_set[10] = '035E73';
+        color_set[11] = '03A688';
+        color_set[12] = 'ABD904';
+        color_set[13] = 'F2EA77';
+        color_set[14] = '026BB5';
+        color_set[15] = '00AAE3';
+        color_set[16] = 'D93D52';
+        color_set[17] = '593A71';
+        color_set[18] = '43A19F';
+        color_set[19] = 'F58E03';
+        color_set[20] = 'FF5300';
+        color_set[21] = 'E81402';
+        color_set[22] = '80064D';
+        color_set[23] = '450459';
+
+
+function get_random_color() {
+    var color = '#';
+        color += color_set[Math.floor(Math.random()*color_set.length)];
+    return color;
+}
+
